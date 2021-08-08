ebaf8e22af07c7bee1db7588d86e4cd121e4ff33
Mert Karaman <mertkrmn06@gmail.com>
2021-08-08 13:25:03 +0300

    after spring security and basic auth

--- src/main/java/com/mertkaraman/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/mertkaraman/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,6 +2,7 @@ package com.mertkaraman.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
